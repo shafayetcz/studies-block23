895b9ce144f206f3cacdb189216942372a068cc9
shafayetcz <shafayetcz@gmail.com>
2023-02-22 19:47:00 +0100

    Create nav.php

---
+++ page/nav.php
@@ -0,0 +1,396 @@
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+       
+        <div class="sidebar-brand-text mx-3">Shahensha-a</div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="soft.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+
+      <!-- Divider -->
+	  <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseone" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-pager"></i>
+          <span>website</span>
+        </a>
+        <div id="collapseone" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Fornd page:</h6>
+            <a class="collapse-item" href="../soft/notice.php">Notice</a>
+			<a class="collapse-item" href="../soft/sli.php">Web Slider</a>
+            <a class="collapse-item" href="../soft/hall.php">Hall Of Fame</a>
+			<a class="collapse-item" href="../soft/cn.php">Class Notice</a>
+          </div>
+        </div>
+      </li>
+      <hr class="sidebar-divider">
+        
+      <!-- Heading -->
+      <div class="sidebar-heading">
+	  <i class="fas fa-info"></i>
+      Information
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-user-graduate"></i>
+          <span>student</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Components:</h6>
+            <a class="collapse-item" href="../soft/sti.php">Student Admition</a>
+            <a class="collapse-item" href="../soft/stv.php">Student Information</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-chalkboard-teacher"></i>
+          <span>Teacher</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="../soft/treg.php">Teacher Registration</a>
+            <a class="collapse-item" href="../soft/tlist.php">Teacher List</a>
+          </div>
+        </div>
+      </li>
+      <!-- Divider -->
+	     <hr class="sidebar-divider">
+	    <div class="sidebar-heading">
+	  <i class="fas fa-diagnoses"></i>
+      EXAIM
+      </div>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-marker"></i>
+          <span>Mark Entry</span>
+        </a>
+        <div id="collapse1" class="collapse" aria-labelledby="heading1" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Components:</h6>
+            <a class="collapse-item" href="../soft/stmark.php?m='i'">Class One</a>
+			<a class="collapse-item" href="../soft/stmark.php?m='ii'">Class Tow</a>
+			<a class="collapse-item" href="../soft/stmark.php?m='iii'">Class Three</a>
+             <a class="collapse-item" href="../soft/stmark.php?m='iv'">Class Four</a>
+			<a class="collapse-item" href="../soft/stmark.php?m='v'">Class Five</a>
+			<a class="collapse-item" href="../soft/stmark.php?m='vi'">Class Six</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+	     <li class="nav-item">
+          <a class="nav-link" href="result.php">
+         <i class="fas fa-poll-h"></i>
+          <span>Result</span></a>
+         </li>
+	  <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse3" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-scroll"></i>
+          <span>Mark sheet</span>
+        </a>
+        <div id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="../soft/pdf/pdf.php">ALL CLASS </a>
+            <a class="collapse-item" href="../soft/pdf/pdf1.php"> CLASS ONE </a>
+            <a class="collapse-item" href="../soft/pdf/pdf2.php">CLASS TWO </a>
+            <a class="collapse-item" href="../soft/pdf/pdf3.php">CLASS THREE</a>
+			<a class="collapse-item" href="../soft/pdf/pdf4.php">CLASS FOUR</a>
+			<a class="collapse-item" href="../soft/pdf/pdf5.php">CLASS FIVE</a>
+          </div>
+        </div>
+      </li>
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+     
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages1" aria-expanded="true" aria-controls="collapsePages">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>EXPENSE</span>
+        </a>
+        <div id="collapsePages1" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="fee1.php">Account</h6>
+			<a class="collapse-item" href="expense.php">Expance Entry </a>
+			<button type="button" class="btn btn-light ba " data-toggle="modal" data-target="#myModal4">Expance Information</button>
+			<button type="button" class="btn btn-light ba " data-toggle="modal" data-target="#myModal">Fee Sheet</button>
+            <div class="collapse-divider"></div>
+            <h6 class="collapse-header">Other Pages:</h6>
+            <button type="button" class="btn btn-light ba " data-toggle="modal" data-target="#myModal2">Paid Student</button>
+            <button type="button" class="btn btn-light ba " data-toggle="modal" data-target="#myModal3">Upaid Student</button>
+          </div>
+        </div>
+      </li>
+
+      <!-- Nav Item - Tables -->
+      <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Tables</span></a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="logout.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Logout</span></a>
+      </li>
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+	<div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Heading</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+<form class="form-inline" action="pdf/fss.php" target="null" method="post">
+  <label>Month:- </label>
+    <select name="month">
+    <option value="January">January</option>
+    <option value="February">February</option>
+	<option value="March">March</option>
+    <option value="April">April </option>
+	<option value="May">May </option>
+    <option value="June">June</option>
+    <option value="July ">July  </option>
+    <option value="August">August  </option>
+	<option value="September ">September </option>
+    <option value="October">October </option>
+    <option value="November">November  </option>
+    <option value="December">December  </option>
+  </select>
+  <label style="padding-left: 20px;">Password:- </label>
+    <select name="class">
+    <option value="i">i</option>
+    <option value="ii">ii</option>
+    <option value="iii">iii</option>
+    <option value="iv">iv</option>
+	<option value="v">v</option>
+  </select>
+  <p style="padding-left: 20px;"></p>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <!-- Modal Fee Entry -->
+  	<div class="modal fade" id="myModal1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Heading</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+<form class="form-inline" action="../soft/fe.php" target="null" method="post">
+  <label>Month:- </label>
+    <select name="month">
+    <option value="January">January</option>
+    <option value="February">February </option>
+    <option value="March">March </option>
+    <option value="April">April </option>
+	<option value="May">May </option>
+    <option value="June">June</option>
+    <option value="July ">July  </option>
+    <option value="August">August  </option>
+	<option value="September ">September </option>
+    <option value="October">October </option>
+    <option value="November">November  </option>
+    <option value="December">December  </option>
+  </select>
+  <label style="padding-left: 20px;">Password:- </label>
+    <select name="class">
+    <option value="i">i</option>
+    <option value="ii">ii</option>
+    <option value="iii">iii</option>
+    <option value="iv">iv</option>
+	<option value="v">v</option>
+  </select>
+  <p style="padding-left: 20px;"></p>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+    <!-- Modal Paid s -->
+    	<div class="modal fade" id="myModal2">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Paid Student</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+<form class="form-inline" action="pa.php" target="null" method="post">
+  <label>Month:- </label>
+    <select name="month">
+    <option value="January">January</option>
+    <option value="February">February </option>
+    <option value="March">March </option>
+    <option value="April">April </option>
+	<option value="May">May </option>
+    <option value="June">June</option>
+    <option value="July">July  </option>
+    <option value="August">August  </option>
+	<option value="September">September </option>
+    <option value="October">October </option>
+    <option value="November">November  </option>
+    <option value="December">December  </option>
+  </select>
+  <label style="padding-left: 20px;">Password:- </label>
+    <select name="class">
+    <option value="i">i</option>
+    <option value="ii">ii</option>
+    <option value="iii">iii</option>
+    <option value="iv">iv</option>
+	<option value="v">v</option>
+  </select>
+  <p style="padding-left: 20px;"></p>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  
+      <!-- Modal uPaid s -->
+    	<div class="modal fade" id="myModal3">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Unpaid student</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+<form class="form-inline" action="upa.php" target="null" method="post">
+  <label>Month:- </label>
+    <select name="month">
+    <option value="January">January</option>
+    <option value="February">February </option>
+    <option value="March">March </option>
+    <option value="April">April </option>
+	<option value="May">May </option>
+    <option value="June">June</option>
+    <option value="July">July  </option>
+    <option value="August">August  </option>
+	<option value="September">September </option>
+    <option value="October">October </option>
+    <option value="November">November  </option>
+    <option value="December">December  </option>
+  </select>
+  <label style="padding-left: 20px;">Password:- </label>
+    <select name="class">
+    <option value="i">i</option>
+    <option value="ii">ii</option>
+    <option value="iii">iii</option>
+    <option value="iv">iv</option>
+	<option value="v">v</option>
+  </select>
+  <p style="padding-left: 20px;"></p>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+        <!-- Modal expan  -->
+    	<div class="modal fade" id="myModal4">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Unpaid student</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+<form class="form-inline" action="showex.php" target="null" method="post">
+  <label>Month:- </label>
+    <select name="month">
+    <option value="January">January</option>
+    <option value="February">February </option>
+    <option value="March">March </option>
+    <option value="April">April </option>
+	<option value="May">May </option>
+    <option value="June">June</option>
+    <option value="July">July  </option>
+    <option value="August">August  </option>
+	<option value="September">September </option>
+    <option value="October">October </option>
+    <option value="November">November  </option>
+    <option value="December">December  </option>
+  </select>
+  <label style="padding-left: 20px;">Password:- </label>
+    <select name="class">
+    <option value="i">i</option>
+    <option value="ii">ii</option>
+    <option value="iii">iii</option>
+    <option value="iv">iv</option>
+	<option value="v">v</option>
+  </select>
+  <p style="padding-left: 20px;"></p>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+        </div>
+
+      </div>
+    </div>
+  </div>
