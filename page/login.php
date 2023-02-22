abc1c6b142502edbb5cf60ff9c4df5d295455758
shafayetcz <shafayetcz@gmail.com>
2023-02-22 19:46:57 +0100

    Create login.php

---
+++ page/login.php
@@ -0,0 +1,22 @@
+<?php session_start()?>
+<?php include('db/connection.php') ?>
+<?php
+if(isset($_POST['submit'])){
+	$username=$_POST['username'];
+	$email=$_POST['email'];
+	$password=$_POST['password'];
+	
+	$select_query="SELECT * FROM Teacher WHERE name='$username' && email='$email' && pass='$password'";
+	$run_query=mysqli_query($connect,$select_query);
+	$count=mysqli_num_rows($run_query);
+	if($count>0){
+		header('location:http://localhost/school1/sec/Teacher/soft.php');
+		$_SESSION['name']=$username;
+		$_SESSION['email']=$email;
+		$_SESSION['pass']=$password;
+	}
+	else{
+		header('location:http://localhost/school1/sec/soft/page/er.php');
+	}
+}
+?>
